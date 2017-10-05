ff80af90180efa00c3321cb1e767d5600f81bba3
James Usrey <jusrey@mit.edu>
2017-10-04 21:17:26 -0500

    Initial submission
    Stupid little game.

---
+++ gamemodel.js
@@ -0,0 +1,240 @@
+//does f i times.
+times = function (i, f) {
+	if (i === 0) return;
+	f(); times (i-1, f)
+	}
+
+var XDIM = 32;
+var YDIM = 16;
+
+//Initializes with XDIM x YDIM blank grid
+
+
+Game = function() {
+    var that = Object.create(Game.prototype);
+    
+    that.score = 0;
+    that.health = 10;
+
+    var cl = [];
+    cl.push(XDIM/2);
+    cl.push(YDIM/2);
+
+    that.curLocation = cl
+
+
+    var rows = []
+	 times(XDIM, function() {
+  		var col = [];
+  		rows.push(col);
+  		times(YDIM, function() {
+    		col.push(0);
+  		})
+	})
+
+  rows[XDIM/2][YDIM/2] = 2; 
+
+	that.model = rows;
+
+  that.message = "Weclome to the game.";
+  
+ 
+
+    return that;
+}
+
+//colors 0 = dead, 1 = monster, 2 = player, 3 = fire, 4 = cheat
+
+
+
+Game.prototype = {
+
+    killCell : function(x,y) { 
+    	this.model[x][y] = 0;
+      },
+
+    burnCell : function(x,y) { 
+      oldtype = this.model[x][y];
+      this.model[x][y] = 3;
+      if (oldtype === 1){
+        this.message = "You Froosh'd and/or Dooj'd the enemy monster. Plus 1 point."
+        this.changeScore(1);
+      } else if (oldtype === 4){
+        this.message = "Oh no, you Froosh'd and/or Dooj'd a The Cheat. Minus 15 points";
+        this.changeScore(-15);
+      } else if (oldtype === 3){
+        this.message = "Fighting fire with fire, eh? No point, so no points.";
+      } else if (oldtype === 2){
+        alert("You've lit yourself on fire somehow. GAME OVER, I guess?");
+        location.reload();
+      } else if (oldtype === 100){
+        alert("You found the secret SECRET COLLECT easter egg! Now, it's a secret SECRET COLLECT omelette. Nice going, brain for brains.");
+      } 
+
+
+
+
+      },
+
+    placeMonster : function(x,y) { 
+      this.model[x][y] = 1;
+      },
+
+    placeCheat : function(x,y) {
+      this.model[x][y] = 4;
+    },
+
+    placeSECRET(x,y){
+      this.model[x][y] = 100;
+    },
+
+    changeScore : function(amt){
+      this.score = this.score + amt;
+      if (this.score >= 99999999999999999){
+        alert("FLAGRANT SYSTEM ERROR \n \n ...I guess that means you win?");
+      }
+      if (this.score <= -999999999999999){
+        alert("FLAGRANT SYSTEM ERROR \n \n ...I guess that means you lose?");
+      }  
+
+      if (this.score === 423827){
+        alert("WAAGH! That is not a small number! That is a BIG number!");
+      } 
+    },
+
+    injure : function(amt){
+      this.health = this.health - amt;
+      if (this.health <= 0){
+        alert("You died. GAME OVER");
+        location.reload();
+      }
+    },
+    
+    resetZero : function() {
+    	this.model = Game().model;
+      this.curLocation = Game().curLocation;
+      this.score = Game().score;
+      },
+
+    moveUp : function(){
+
+      var curX = this.curLocation[0];
+      var curY = this.curLocation[1];
+      var newY = (curY+YDIM -1)%YDIM;
+      var oldtype = this.model[curX][newY];
+      if (oldtype === 1){
+        this.message ="Ran into a monster. -1 HP.";
+        this.injure(1);
+      } else if (oldtype === 4){
+        this.message ="You kick The Cheat. Plus 10 points.";
+        this.changeScore(10);
+      }else if (oldtype === 3){
+        this.message ="Put out fire with your face. Minus 3 health.";
+        this.injure(3);
+      }else if (oldtype === 2){
+        this.message ="Somehow stepped on owndangself. Plus 1,000,000,000 points! (but -1 health)";
+        this.changeScore(1000000);
+        this.injure(1);
+      }else if (oldtype === 100){
+        alert("Found the secret SECRET COLLECT easter egg!") 
+
+      }
+
+
+      this.model[curX][curY] = 0;
+      this.curLocation[1] = newY;
+      this.model[curX][newY] = 2;
+    },
+
+    moveDown : function(){
+      var curX = this.curLocation[0];
+      var curY = this.curLocation[1];
+      var newY = (curY+YDIM +1)%YDIM;
+      var oldtype = this.model[curX][newY];
+      if (oldtype === 1){
+        this.message ="Ran into a monster. -1 HP.";
+        this.injure(1);
+      } else if (oldtype === 4){
+        this.message ="You kick The Cheat. Plus 10 points.";
+        this.changeScore(10);
+      }else if (oldtype === 3){
+        this.message ="Put out fire with your face. Minus 3 health.";
+        this.injure(3);
+      }else if (oldtype === 2){
+        this.message ="Somehow stepped on owndangself. Plus 1,000,000,000 points! (but -1 health)";
+        this.changeScore(1000000);
+        this.health =  this.health -1;
+      }else if (oldtype === 100){
+        alert("Found the secret SECRET COLLECT easter egg!") 
+
+      }
+
+
+      this.model[curX][curY] = 0 ;
+      this.curLocation[1] = newY;
+      this.model[curX][newY] = 2;
+    },
+
+    moveLeft : function(){
+      var curX = this.curLocation[0];
+      var curY = this.curLocation[1];
+      var newX = (curX+XDIM -1)%XDIM;
+      var oldtype = this.model[newX][curY];
+      if (oldtype === 1){
+        this.message ="Ran into a monster. -1 HP.";
+        this.injure(1);
+      } else if (oldtype === 4){
+        this.message ="You kick The Cheat. Plus 10 points.";
+        this.changeScore(10);
+      }else if (oldtype === 3){
+        this.message ="Put out fire with your face. Minus 3 health.";
+        this.injure(3);
+      }else if (oldtype === 2){
+        this.message ="Somehow stepped on owndangself. Plus 1,000,000,000 points! (but -1 health)";
+        this.changeScore(1000000);
+        this.injure(1);
+      }else if (oldtype === 100){
+        alert("Found the secret SECRET COLLECT easter egg!") 
+
+      }
+
+
+      this.model[curX][curY] = 0 ;
+      this.curLocation[0] = newX;
+      this.model[newX][curY] = 2;
+    },
+
+    moveRight : function(){
+      var curX = this.curLocation[0];
+      var curY = this.curLocation[1];
+      var newX = (curX+XDIM +1)%XDIM;
+      var oldtype = this.model[newX][curY];
+      if (oldtype === 1){
+        this.message ="Ran into a monster. -1 HP.";
+        this.injure(1);
+      } else if (oldtype === 4){
+        this.message ="You kick The Cheat. Plus 10 points.";
+        this.changeScore(10);
+      }else if (oldtype === 3){
+        this.message ="Put out fire with your face. Minus 3 health.";
+        this.injure(3);
+      }else if (oldtype === 2){
+        this.message ="Somehow stepped on owndangself. Plus 1,000,000,000 points! (but -1 health)";
+        this.changeScore(1000000);
+        this.injure(1);
+      }else if (oldtype === 100){
+        alert("Found the secret SECRET COLLECT easter egg!") 
+
+      }
+
+
+      this.model[curX][curY] = 0 ;
+      this.curLocation[0] = newX;
+      this.model[newX][curY] = 2;
+    },
+    
+
+
+
+
+}
---
+++ gamewidget.js
@@ -0,0 +1,271 @@
+var gamemodel = Game();
+var scale = 25;
+
+
+//draws the model onto a canvas
+function drawGrid() {
+
+var can = document.getElementById("Grid");
+var canM = can.getContext("2d");
+canM.clearRect(0,0,scale*XDIM,scale*YDIM);
+//console.log(lifemodel.model[0][0]);
+for (var i=0; i < XDIM; i++){
+	for (var j=0; j < YDIM; j++){
+		if (gamemodel.model[i][j] === 1){
+			canM.fillStyle = "#0000FF";
+			canM.fillRect(scale*i,scale*j, scale, scale);
+		}
+		if (gamemodel.model[i][j] === 2){
+			canM.fillStyle = "#00BEEF";
+			canM.fillRect(scale*i,scale*j, scale/2 -3, scale);
+			canM.fillRect(scale*i + (scale/2) +3, scale*j, scale/2 -3, scale);
+			canM.fillStyle = "#000000"
+			canM.fillRect(scale*i,scale*j, scale/2 -3, scale/3)
+			canM.fillRect(scale*i + (scale/2) +3, scale*j, scale/2 -3, scale/3);
+
+		}
+		if (gamemodel.model[i][j] === 3){
+			canM.fillStyle = "#FF4500";
+			canM.fillRect(scale*i,scale*j, scale, scale);
+		}
+
+		if (gamemodel.model[i][j] === 4){
+			canM.fillStyle = "#FFFF00";
+			canM.fillRect(scale*i,scale*j, scale, scale);
+		}
+
+		if (gamemodel.model[i][j] === 100){
+			canM.fillStyle = "#FF0000";
+			canM.fillRect(scale*i,scale*j, scale, scale);
+		}
+
+	}
+}
+
+var scorecan = document.getElementById("score");
+var scorectx = scorecan.getContext("2d");
+scorectx.fillStyle = "#FFFFFF";
+scorectx.fillRect(0,0,800,40);
+scorectx.fillStyle = "#000000"
+scorectx.font = "20px Arial";
+scorectx.fillText("Score: " + gamemodel.score,10,20);
+scorectx.fillText("Health: " + gamemodel.health, 700,20);
+
+var scorecan = document.getElementById("message");
+var scorectx = scorecan.getContext("2d");
+scorectx.fillStyle = "#FFFFFF";
+scorectx.fillRect(0,0,800,40);
+scorectx.fillStyle = "#000000"
+scorectx.font = "20px Arial";
+scorectx.fillText(gamemodel.message,10,20);
+
+
+}
+
+drawGrid();
+
+
+
+// newGame
+function newgame() {
+	gamemodel.resetZero();
+	drawGrid();
+}
+
+// help
+function about() {
+	alert("A crappy fangame made by Aureo Speedwagon. October 4, 2017.")
+}
+
+
+// up
+function up() {
+	gamemodel.moveUp();
+	drawGrid();
+	if (Math.random() > .90){
+		addthing();
+	}
+
+}
+
+// down
+function down() {
+	gamemodel.moveDown();
+	drawGrid();
+	if (Math.random() > .90){
+		addthing();
+	}
+}
+
+// left
+function left() {
+	gamemodel.moveLeft();
+	drawGrid();
+	if (Math.random() > .90){
+		addthing();
+	}
+}
+
+// right
+function right() {
+	gamemodel.moveRight();
+	drawGrid();
+	if (Math.random() > .90){
+		addthing();
+	}
+}
+
+
+// dooj
+function dooj() {
+
+	var curLoc = gamemodel.curLocation;
+	var curx = curLoc[0];
+	var cury = curLoc[1];
+
+	var burncells = [];
+
+	gamemodel.burnCell(curx, cury + 1)
+	gamemodel.burnCell(curx, cury + 2)
+	gamemodel.burnCell(curx, cury + 3)
+	gamemodel.burnCell(curx+1, cury + 2)
+	gamemodel.burnCell(curx-1, cury + 2)
+	gamemodel.burnCell(curx+1, cury + 3)
+	gamemodel.burnCell(curx-1, cury + 3)
+
+	gamemodel.burnCell(curx, cury - 1)
+	gamemodel.burnCell(curx, cury - 2)
+	gamemodel.burnCell(curx, cury - 3)
+	gamemodel.burnCell(curx+1, cury - 2)
+	gamemodel.burnCell(curx-1, cury - 2)
+	gamemodel.burnCell(curx+1, cury - 3)
+	gamemodel.burnCell(curx-1, cury - 3)
+
+	drawGrid();
+
+	setTimeout(function(){
+
+	gamemodel.killCell(curx, cury + 1)
+	gamemodel.killCell(curx, cury + 2)
+	gamemodel.killCell(curx, cury + 3)
+	gamemodel.killCell(curx+1, cury + 2)
+	gamemodel.killCell(curx-1, cury + 2)
+	gamemodel.killCell(curx+1, cury + 3)
+	gamemodel.killCell(curx-1, cury + 3)
+
+	gamemodel.killCell(curx, cury - 1)
+	gamemodel.killCell(curx, cury - 2)
+	gamemodel.killCell(curx, cury - 3)
+	gamemodel.killCell(curx+1, cury - 2)
+	gamemodel.killCell(curx-1, cury - 2)
+	gamemodel.killCell(curx+1, cury - 3)
+	gamemodel.killCell(curx-1, cury - 3)
+
+	drawGrid();
+	if (Math.random() > .90){
+		addthing();
+	}
+
+
+	}, 100);
+
+}
+
+// froosh
+function froosh() {
+
+	var curLoc = gamemodel.curLocation;
+	var curx = curLoc[0];
+	var cury = curLoc[1];
+
+	var burncells = [];
+
+	gamemodel.burnCell(curx+1, cury)
+	gamemodel.burnCell(curx+2, cury)
+	gamemodel.burnCell(curx+3, cury)
+	gamemodel.burnCell(curx+2, cury + 1)
+	gamemodel.burnCell(curx+2, cury - 1)
+	gamemodel.burnCell(curx+3, cury + 1)
+	gamemodel.burnCell(curx+3, cury - 1)
+
+	gamemodel.burnCell(curx-1, cury)
+	gamemodel.burnCell(curx-2, cury)
+	gamemodel.burnCell(curx-3, cury)
+	gamemodel.burnCell(curx-2, cury + 1)
+	gamemodel.burnCell(curx-2, cury - 1)
+	gamemodel.burnCell(curx-3, cury + 1)
+	gamemodel.burnCell(curx-3, cury - 1)
+
+	drawGrid();
+
+	setTimeout(function(){
+
+	gamemodel.killCell(curx+1, cury)
+	gamemodel.killCell(curx+2, cury)
+	gamemodel.killCell(curx+3, cury)
+	gamemodel.killCell(curx+2, cury + 1)
+	gamemodel.killCell(curx+2, cury - 1)
+	gamemodel.killCell(curx+3, cury + 1)
+	gamemodel.killCell(curx+3, cury - 1)
+
+	gamemodel.killCell(curx-1, cury)
+	gamemodel.killCell(curx-2, cury)
+	gamemodel.killCell(curx-3, cury)
+	gamemodel.killCell(curx-2, cury + 1)
+	gamemodel.killCell(curx-2, cury - 1)
+	gamemodel.killCell(curx-3, cury + 1)
+	gamemodel.killCell(curx-3, cury - 1)
+
+	drawGrid();
+	if (Math.random() > .90){
+		addthing();
+	}
+
+
+	}, 100);
+
+}
+
+
+function addthing(){
+	var randx = Math.floor(Math.random()*XDIM);
+	var randy = Math.floor(Math.random()*YDIM);
+	var monvalue = Math.random();
+
+
+	if (monvalue < .2){
+		gamemodel.placeCheat(randx, randy);
+
+	}else if(monvalue >.999){
+		gamemodel.placeSECRET(randx,randy);
+	}else{
+		gamemodel.placeMonster(randx, randy);
+	}
+
+
+
+	drawGrid();
+
+
+
+
+}
+
+
+keydown = function(e){
+ if(e.keyCode==37){
+   document.getElementById("left").click()
+}else if(e.keyCode==38){
+   document.getElementById("up").click()
+}else if(e.keyCode==39){
+   document.getElementById("right").click()
+}else if(e.keyCode==40){
+   document.getElementById("down").click()
+}else if(e.keyCode==68){
+   document.getElementById("dooj").click()
+}else if(e.keyCode==70){
+   document.getElementById("froosh").click()
+}else if(e.keyCode==82){
+   document.getElementById("newgame").click()
+}}
+document.onkeydown = keydown
